849cdf542e6f4551b8228fa5acd718a209ebada7
kunwoo309 <kunwoo309@hotmail.com>
2016-06-01 10:02:33 +1200

    second commit

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "7d59d1b446bc63e720db22cbfe16ff8d"
+  config.secret = "baf9c669e9473adb6097c3db44a06d97"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
